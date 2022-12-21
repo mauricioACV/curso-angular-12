da5e0738db723074d411690c97f61a9614bc74a5
mauricioACV <mauricio.campusano@gmail.com>
2022-12-21 16:55:49 -0300

    Primer commit con avances en generación de componentes, módulos, integración de angular material, rutas y lazy loading

---
+++ src/app/pages/checkout/checkout.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-checkout',
+  templateUrl: './checkout.component.html',
+  styleUrls: ['./checkout.component.scss']
+})
+export class CheckoutComponent {
+  model = {
+    name: '',
+    store: '',
+    shippingAddress: '',
+    city: '',
+  }
+
+  stores = [
+    {
+      "id": 1,
+      "name": "Park Row at Beekman St",
+      "address": "38 Park Row",
+      "city": "New York",
+      "openingHours": "10:00 - 14:00 and 17:00 - 20:30"
+    },
+    {
+      "id": 2,
+      "name": "Store Alcalá",
+      "address": "Calle de Alcalá, 21",
+      "city": "Madrid",
+      "openingHours": "10:00 - 14:00 and 17:00 - 20:30"
+    }
+  ]
+
+  onPickupOrDelivery(value: boolean): void {
+    console.log(value);
+  }
+
+}
